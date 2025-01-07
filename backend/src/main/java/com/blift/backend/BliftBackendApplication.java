909a427d8d3cd076cf8e56b810992a1e2f081c79
Yakubiak Bohdan <bogdan17yit@gmail.com>
2025-01-07 11:29:35 -0500

    Replaced old backend folder with new Spring Boot base project

---
+++ backend/src/main/java/com/blift/backend/BliftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blift.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BliftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BliftBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blift/backend/BliftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blift.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BliftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
